ad3c7ae1e500164ad88ed4040e49fae31fd789f2
aaronk <aaronk@bixly.com>
2021-08-03 14:53:29 -0700

    added refactor

--- letterbox-paint-squad.js
+++ letterbox-paint-squad.js
@@ -1,9 +1,19 @@
+// My Original Solution
 var paintLetterboxes = function(start, end) {
   let res = [0,0,0,0,0,0,0,0,0,0];
   for (let i = start; i <= end; i ++) {
     i.toString().split("").forEach((dig)=>{
       res[parseInt(dig)]++
     });
+  }
+  return res;
+}
+
+// refactor
+var paintLetterboxes = function(start, end) {
+  let res = new Array.fill(0);
+  for (let i = start; i <= end; i ++) {
+    i.toString().split("").forEach( (dig) => res[parseInt(dig)]++ );
   }
   return res;
 }
